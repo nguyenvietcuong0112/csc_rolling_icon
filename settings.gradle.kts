8a24ffe3182a1e105e795c00dd6f2919d484b639
CuongNV1 <cuongnv@cscmobi.com>
2026-07-24 16:11:05 +0700

    fix bugs

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,45 +1,45 @@
-//pluginManagement {
-//    repositories {
-//        google()
-//        mavenCentral()
-//        gradlePluginPortal()
-//        maven { url = uri("https://jitpack.io") }
-//        maven { url = uri("https://maven.google.com") }
-//        maven {
-//            url =
-//                uri("https://dl-maven-android.mintegral.com/repository/mbridge_android_sdk_oversea")
-//        }
-//        maven {
-//            url = uri("https://artifact.bytedance.com/repository/pangle/")
-//        }
-//    }
-//}
-//dependencyResolutionManagement {
-//    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-//    repositories {
-//        google()
-//        mavenCentral()
-//        maven { url = uri("https://jitpack.io") }
-//        maven { url = uri("https://maven.google.com") }
-//        maven {
-//            url =
-//                uri("https://dl-maven-android.mintegral.com/repository/mbridge_android_sdk_oversea")
-//        }
-//        maven {
-//            url = uri("https://artifact.bytedance.com/repository/pangle/")
-//        }
-//        maven {
-//            url = uri("https://maven.pkg.github.com/cscappstudio/csc_ads_lib")
-//            credentials {
-//                username = "csccuong"
-//                password = "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"
-//            }
-//        }
-//        maven { url = uri("https://oss.sonatype.org/content/repositories/snapshots/") }
-//        maven { url = uri("https://central.sonatype.com/repository/maven-snapshots/") }
-//        maven { url = uri("https://oss.sonatype.org/content/repositories/releases/") }
-//    }
-//}
-//
-//rootProject.name = "RollingIcons"
-//include(":app")
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+        maven { url = uri("https://jitpack.io") }
+        maven { url = uri("https://maven.google.com") }
+        maven {
+            url =
+                uri("https://dl-maven-android.mintegral.com/repository/mbridge_android_sdk_oversea")
+        }
+        maven {
+            url = uri("https://artifact.bytedance.com/repository/pangle/")
+        }
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://jitpack.io") }
+        maven { url = uri("https://maven.google.com") }
+        maven {
+            url =
+                uri("https://dl-maven-android.mintegral.com/repository/mbridge_android_sdk_oversea")
+        }
+        maven {
+            url = uri("https://artifact.bytedance.com/repository/pangle/")
+        }
+        maven {
+            url = uri("https://maven.pkg.github.com/cscappstudio/csc_ads_lib")
+            credentials {
+                username = "csccuong"
+                password = "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"
+            }
+        }
+        maven { url = uri("https://oss.sonatype.org/content/repositories/snapshots/") }
+        maven { url = uri("https://central.sonatype.com/repository/maven-snapshots/") }
+        maven { url = uri("https://oss.sonatype.org/content/repositories/releases/") }
+    }
+}
+
+rootProject.name = "RollingIcons"
+include(":app")
