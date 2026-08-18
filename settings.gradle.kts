6f4da3738e647acb6fa1ed883b3682b682533c0e
cuong <cuongnv@cscmobi.com>
2026-08-18 15:59:11 +0700

    fix(crash-anr): resolve all 6 crashes, optimize physics and icon loading, update onboarding wallpaper flow

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -28,13 +28,13 @@ dependencyResolutionManagement {
         maven {
             url = uri("https://artifact.bytedance.com/repository/pangle/")
         }
-//        maven {
-//            url = uri("https://maven.pkg.github.com/cscappstudio/csc_ads_lib")
-//            credentials {
-//                username = ""
-//                password = ""
-//            }
-//        }
+        maven {
+            url = uri("https://maven.pkg.github.com/cscappstudio/csc_ads_lib")
+            credentials {
+                username = "csccuong"
+                password = "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"
+            }
+        }
         maven { url = uri("https://oss.sonatype.org/content/repositories/snapshots/") }
         maven { url = uri("https://central.sonatype.com/repository/maven-snapshots/") }
         maven { url = uri("https://oss.sonatype.org/content/repositories/releases/") }
